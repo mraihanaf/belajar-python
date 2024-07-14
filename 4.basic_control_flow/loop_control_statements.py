2883654783b0f45cfc5423e2ee8b7cac92084321
mraihanaf <mraihanaf0@gmail.com>
2024-07-14 21:14:57 +0700

    add loop_control_statement.py

---
+++ 4.basic_control_flow/loop_control_statements.py
@@ -0,0 +1,22 @@
+# Loop control statements
+#
+# Break
+
+for i in range(5):
+    if i ==  3: break
+    print(i,"break")
+
+# Continue
+
+for i in range(5):
+    if i == 3:
+        print("Continue")
+        continue
+    print(i,"continue")
+
+# Pass
+# pass does nothing.
+print("pass","Tekan CTRL-C Untuk Keluar")
+while True:
+    pass
+
